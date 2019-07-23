476e9bc4b23a843d0a4d84d805c69738de523bf1
Peter Walker <25408515+peteawalk@users.noreply.github.com>
2019-07-23 07:26:16 -0500

    initial upload: updates to all files

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBY7O96YIxd_QG0uKlgeejNuCJE6UYb_JQ",
+    authDomain: "trainscheduler-eb79e.firebaseapp.com",
+    databaseURL: "https://trainscheduler-eb79e.firebaseio.com",
+    projectId: "trainscheduler-eb79e",
+    storageBucket: "",
+    messagingSenderId: "96747496347",
+    appId: "1:96747496347:web:80cc3aa0012f1d17"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
